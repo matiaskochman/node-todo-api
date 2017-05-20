9410cc0cf36e4042c64ba0efd05f64294c7f51fc
matias <matiaskochman@gmail.com>
2017-05-20 14:59:05 +0200

    setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 //mongoose.Promise = require('bluebird');
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
   mongoose
